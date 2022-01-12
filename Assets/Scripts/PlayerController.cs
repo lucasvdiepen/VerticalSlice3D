135a46f279c77984595acb27ef966fbc9fa1d362
Bart <bartdb2001@gmail.com>
2022-01-12 15:24:57 +0100

    Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //public int speed
-    //public int maxvelocity
+    public float maxVelocity = 10f;
+    public float speed = 1f;
+    private bool isGrounded;
+    public float jumpFactor = 6f;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         if (Input.GetKey(KeyCode.A))
-            rb.AddForce(Vector3.left);
+            rb.AddForce(Vector3.left * speed);
         if (Input.GetKey(KeyCode.D))
-            rb.AddForce(Vector3.right);
+            rb.AddForce(Vector3.right * speed);
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce(Vector3.forward);
+            rb.AddForce(Vector3.forward * speed);
         if (Input.GetKey(KeyCode.S))
-            rb.AddForce(Vector3.back);
+            rb.AddForce(Vector3.back * speed);
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+            rb.AddForce(Vector3.up * speed * jumpFactor);
+
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        }
     }
 }
